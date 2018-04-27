c82d33378698de77b3bb9fbc844fb964fd0f6e53
alo456 <francisco.alavezs25@anahuac.mx>
2018-04-26 19:22:35 -0500

    Vistas pt1

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Cookie;
@@ -77,11 +79,193 @@ class AdminController extends Controller
     }
     
     public function personal(Request $request){
-        return $this->render('Administrator/personal.html.twig');
+        $data = array();
+         $form = $this->createFormBuilder($data)
+        ->add('clientName', TextType::class, array ('label' => 'Nombre',
+                                                                'attr' => array(
+                                                                    'class' =>'form-control',
+                                                                    'placeholder' => 'Nombre'
+                                                                )
+            ))
+       ->add('clientSecondName', TextType::class, array ('label' => 'Apellido',
+                                                                'attr' => array(
+                                                                    'class' =>'form-control',
+                                                                    'placeholder' => 'Apellido'
+                                                                )
+            ))        
+        ->add('clientEmail', TextType::class, array ('label' => 'Email',
+                                                                'attr' => array(
+                                                                    'class' =>'form-control',
+                                                                    'placeholder' => 'Email'
+                                                                )
+            ))
+        ->add('submit', SubmitType::class, array('label' => 'Guardar',
+                                                                'attr' => array(
+                                                                    'class' =>'btn btn-primary px-4'
+                                                                )
+                ))
+        ->getForm();
+         
+    $form->handleRequest($request);
+    
+    if ($form->isSubmitted() && $form->isValid()) {
+        $data = $form->getData();
+        $headers = array('Accept' => 'application/json');
+        $body = Body::form($data);
+     
+        $responseAPI = RequestAPI::post('http://localhost/taiuniversityapi/public/admin/login',$headers,$body);
+        $body  = $responseAPI->body;
+        if($body->status == 'OK'){ 
+            $token = $responseAPI->headers['Set-Cookie'];
+            $cookie = Cookie::fromString($token);
+            //RequestAPI::cookie($cookie);
+            $response->headers->setCookie($cookie);
+            $response->send();
+            //var_dump($cookie);
+            return $this->redirectToRoute('dashboard', array(
+                                                'TOKEN' => $cookie
+            )); 
+        }else{
+            echo($body->message);
+        }
+    }
+    
+    $personal = array();
+    
+    $personalData = $this->createFormBuilder($personal)
+        ->add('cellphone', TextType::class, array ('label' => 'Celular',
+                                                                'attr' => array(
+                                                                    'class' =>'form-control',
+                                                                    'placeholder' => 'Celular'
+                                                                )
+            ))
+       ->add('birthday', DateType::class, array ('label' => 'Fecha de Nacimiento',
+                                                                'attr' => array(
+                                                                    'class' =>'form-control'
+                                                                )
+            ))        
+        ->add('address', TextType::class, array ('label' => 'Calle y número',
+                                                                'attr' => array(
+                                                                    'class' =>'form-control',
+                                                                    'placeholder' => 'Calle #'
+                                                                )
+            ))
+            
+        ->add('colony', TextType::class, array ('label' => 'Colonia y municipio',
+                                                                'attr' => array(
+                                                                    'class' =>'form-control',
+                                                                    'placeholder' => 'Colonia '
+                                                                )
+            ))
+        ->add('state', TextType::class, array ('label' => 'Estado',
+                                                                'attr' => array(
+                                                                    'class' =>'form-control',
+                                                                    'placeholder' => 'Estado'
+                                                                )
+            ))
+        ->add('submit', SubmitType::class, array('label' => 'Guardar',
+                                                                'attr' => array(
+                                                                    'class' =>'btn btn-primary px-4'
+                                                                )
+                ))
+        ->getForm();
+     
+    $personalData->handleRequest($request);
+    
+    
+    
+    
+    $response = $this->render('Administrator/personal.html.twig', array(
+            'form' => $form->createView(),
+            'personalData' => $personalData->createView()
+            ));
+    
+        return $response;
     }
     
     public function settings(Request $request){
-        return $this->render('Administrator/settings.html.twig');
+        $data = array();
+         $form = $this->createFormBuilder($data)
+        ->add('oldpassword', PasswordType::class, array ('label' => 'Contraseña Actual',
+                                                                'attr' => array(
+                                                                    'class' =>'form-control',
+                                                                    'placeholder' => 'Password'
+                                                                )
+            ))
+       ->add('newpassword', RepeatedType::class, array (
+                                                                'type' => PasswordType::class,
+                                                                'first_options'  => array('label' => 'New Password',  'attr' => array(
+                                                                    'class' =>'form-control',
+                                                                    'placeholder' => 'New Password'
+                                                                )),
+                                                                'second_options' => array('label' => 'Repeat Password', 'attr' => array(
+                                                                    'class' =>'form-control',
+                                                                    'placeholder' => ' Confirm Password'
+                                                                ))        
+            ))        
+        ->add('submit', SubmitType::class, array('label' => 'Guardar',
+                                                                'attr' => array(
+                                                                    'class' =>'btn btn-primary px-4'
+                                                                )
+                ))
+        ->getForm();
+         $form->handleRequest($request);
+     
+    if ($form->isSubmitted() && $form->isValid()) {
+        $data = $form->getData();
+        $headers = array(
+                                'Accept' => 'application/json'
+            );
+        $data['action'] = 'changePassword';
+        $body = Body::form($data);
+        requestAPI::cookie("TOKEN=" . $request->cookies->get('TOKEN'));
+        $responseAPI = RequestAPI::post('http://localhost/taiuniversityapi/public/admin',$headers,$body);
+        $body  = $responseAPI->body;
+//        var_dump($body);
+//        var_dump($body->status);
+        if(isset($body->status) && $body->status == 'OK'){
+//            $token = $responseAPI->headers['Set-Cookie'];
+//            $cookie = Cookie::fromString($token);
+//            //RequestAPI::cookie($cookie);
+//            $response->headers->setCookie($cookie);
+//            $response->send();
+            
+        }else{
+            echo("Credenciales Inválidas");
+        }
+    }
+        
+    
+    $cafeteria = array();
+         $configuration = $this->createFormBuilder($cafeteria)
+        ->add('fromCafe', TextType::class, array ('label' => 'Copiar configuración de:',
+                                                                'attr' => array(
+                                                                    'class' =>'form-control',
+                                                                    'placeholder' => 'Cafeteria'
+                                                                )
+            ))
+       ->add('toCafe', TextType::class, array ('label' => 'a:',
+                                                                'attr' => array(
+                                                                    'class' =>'form-control',
+                                                                    'placeholder' => 'Cafeteria'
+                                                                    )
+            ))        
+        ->add('submit', SubmitType::class, array('label' => 'Copiar configuración',
+                                                                'attr' => array(
+                                                                    'class' =>'btn btn-outline-primary col-auto'
+                                                                )
+                ))
+                 
+        ->getForm();
+         $configuration->handleRequest($request);
+    
+    $response = $this->render('Administrator/settings.html.twig', array(
+            //'last_username' => $lastUsername,
+            'form' => $form->createView(),
+            'configuration' => $configuration->createView()
+            ));
+    
+        return $response;
     }
     
     public function activity(Request $request){
